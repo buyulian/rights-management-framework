a9b6b6f07894739b1abf31bee1fa72f7979a104e
buyulian <67764256@qq.com>
2017-06-04 20:00:08 +0800

    格式化代码，增加了注释

--- src/main/java/com/me/ssm/System/AuthenticationFilter.java
+++ src/main/java/com/me/ssm/System/AuthenticationFilter.java
@@ -7,9 +7,11 @@ package com.me.ssm.System;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 
-//导入必需的 java 库
-//这个过滤器负责总体的权限管理，是从宏观角度观测的
-//实现 Filter 类
+/**
+ * 导入必需的 java 库
+ * 这个过滤器负责总体的权限管理，是从宏观角度观测的
+ * 实现 Filter 类
+ */
 public class AuthenticationFilter implements Filter {
     public void init(FilterConfig config) throws ServletException {
 
@@ -18,17 +20,21 @@ public class AuthenticationFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws java.io.IOException, ServletException {
 
         String url = ((HttpServletRequest) request).getRequestURI();
+        String query = ((HttpServletRequest) request).getQueryString();
 
         //此处写总体的url权限控制代码
 
+        if (query != null) {
+            url += "?" + query;
+        }
         System.out.println(url);
         if (url.startsWith("/")) {
             // 把请求传回过滤链
             chain.doFilter(request, response);
         }
     }
-
+    /** 在 Filter 实例被 Web 容器从服务移除之前调用 */
     public void destroy() {
-        /* 在 Filter 实例被 Web 容器从服务移除之前调用 */
+
     }
 }
